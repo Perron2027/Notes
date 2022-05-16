180ff51937690e8c7466cd8b9a7300eef13a4d6f
hislittlecuzin <hislittlecuzingames@gmail.com>
2022-05-16 17:59:51 -0400

    Completed day 1 Quiz

---
+++ week1/mon/assignments/BreakoutAssignment/src/Main/Java/com/Revature/UI/UI.java
@@ -0,0 +1,17 @@
+//Alec Guarino
+//Jeff Sturgeon
+//Ryan Jacobo
+//Victoria Trotter
+
+package com.Revature.UI;
+
+public class UI {
+    public static void main (String[] args){
+        Solution mySol = new Solution();
+
+        System.out.println( mySol.sumOfTwoNums(5, 6) );
+        System.out.println( mySol.isEven(5) );
+        System.out.println( mySol.FinalBoss("1234567890") );
+    }
+
+}
