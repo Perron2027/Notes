38abfca8ecd5c5742fac8ad31e81417b46fe44cd
EBTW99 <31940176+EBTW99@users.noreply.github.com>
2022-05-16 17:54:03 -0400

    Adding modified files for myDemo java files

---
+++ week1/mon/myDemo/src/main/java/com/revature/demo/Main.java
@@ -0,0 +1,44 @@
+/*package com.revature.demo;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 100;
+        char b = 'c';
+        short c = 0;
+        long d = 0;
+        double e = 0.0;
+        float f = 0.0f;
+        boolean g = false;
+
+        String s = "Hello";
+
+
+        System.out.println("Hello World!");
+
+        if (a % 2 == 0 ) {
+            System.out.println("EVEN!");
+        } else {
+            System.out.println("ODD!");
+        }
+
+        System.out.println(evenOdd(a));
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+        int l = 10;
+        while (l > 0 ) {
+            System.out.println("Hello World!");
+        }
+
+
+    }
+
+    public static String evenOdd(int a) {
+        if (a % 2 == 0) return "EVEN";
+
+        return "ODD";
+    }
+} */
--- week1/mon/myDemo/src/main/java/com/revature/demo/UI.java
+++ week1/mon/myDemo/src/main/java/com/revature/demo/UI.java
@@ -1,2 +1,10 @@
-package com.revature.demo;public class UI {
+package com.revature.demo;
+
+public class UI {
+    public static void main(String[] args) {
+        Dog mydog = new Dog("Aegon", "Pitbull", 3, 'm', 'L');
+
+        System.out.println(mydog);
+    }
+
 }
