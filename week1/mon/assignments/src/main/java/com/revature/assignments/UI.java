815fc4356cf174b3021ce1a5e56c3b8a7ceb6941
MrNoms <tiddle96@gmail.com>
2022-05-16 18:11:09 -0700

    removing redundant directory

--- week1/mon/assignments/src/main/java/com/revature/assignments/UI.java
+++
@@ -1,9 +0,0 @@
-package com.revature.assignments;
-
-public class UI {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        System.out.println(sol.sumOfTwoNum(10, 20));
-    }
-}
