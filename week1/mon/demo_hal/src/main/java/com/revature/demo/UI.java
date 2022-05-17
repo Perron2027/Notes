2668566ee68c339046192879f36f1f25e2e68068
Hal Coronado <hallie_coronado1@baylor.edu>
2022-05-16 21:19:17 -0500

    challenged

---
+++ week1/mon/demo_hal/src/main/java/com/revature/demo/UI.java
@@ -0,0 +1,13 @@
+package com.revature.demo;
+
+public class Main {
+    public static void main(String[] args){
+        int a = 3;
+        if (a %2== 0) {
+            System.out.println("Even!");
+
+        } else {
+            System.out.println("Odd!");
+        }
+    }
+}
