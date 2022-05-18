a4bbe8b1849bbd17afe170640bcac1074edeb7d1
bduong0929 <bao.duong@revature.com>
2022-05-18 09:43:21 -0600

    added exception and inheritance demo to week1/wed

---
+++ week1/wed/demo/src/main/java/com/revature/inheritance/Triangle.java
@@ -0,0 +1,16 @@
+package com.revature.inheritance;
+
+public class Triangle extends Shape {
+
+    /* Rectangle now have access to Shape's methods because it's a child of Shape. */
+    @Override
+    protected double getArea() {
+        /* We still need to implement the functionality of this method for this shape. */
+        return 0;
+    }
+
+    @Override
+    protected double getPerimeter() {
+        return 0;
+    }
+}
---
+++ week1/wed/demo/src/main/java/com/revature/inheritance/UI.java
@@ -0,0 +1,7 @@
+package com.revature.inheritance;
+
+public class UI {
+    public static void main(String[] args) {
+        Pet myCat = new Cat(5, 's', "persian", "garfield");
+    }
+}
